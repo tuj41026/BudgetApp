7d69360faf31fe395e99353bb873221564a81862
tuj41026 <tuj41026@temple.edu>
2022-10-14 23:56:06 -0400

    Home Screen Activity Added

---
+++ app/src/main/java/com/example/budgetapp0/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.budgetapp0.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Settings");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
